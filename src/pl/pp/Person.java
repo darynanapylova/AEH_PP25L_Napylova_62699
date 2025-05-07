29c8d6bff33d05280b70f936870e9ffd46bed415
Lab1 <napylda5252_aehit@students.vizja.pl>
2025-05-07 19:16:39 +0300

    Pierwszy zapis do repozytorium Lab9

--- src/pl/pp/Person.java
+++ src/pl/pp/Person.java
@@ -2,11 +2,9 @@ package pl.pp;
 
 public class Person {
     // pola (atrybuty) klasy określającej obiekt "Person" (jego parametry/cechy)
-    public String forename; // pole przechowujące cechę imię
-    public String surname; // pole przechowujące cechę nazwisko
-    public int age; // pole przechowujące cechę wiek
-    public String city; // pole przechowujące cechę miasto
-    public String sign; // pole przechowujące cechę znak zodiaku
+    private String forename; // pole przechowujące cechę imię
+    private String surname; // pole przechowujące cechę nazwisko
+    private int age; // pole przechowujące cechę wiek
 
     /**
      * Konstruktor domyślny - element inicjalizujący pola obiektu podczas jego tworzenia
@@ -17,37 +15,45 @@ public class Person {
     }
 
     /**
-     * Konstruktor z trzema parametrami, które możemy podać przy jego tworzeniu
+     * Konstruktor z trzema parametrami, które możemy podać przy jego tworzeniu,
+     * wykorzystanie "this" pozwala m.in. na skorzystanie z tych samych nazw
+     * pól i zmiennych
      */
-    public Person(String initForename, String initSurname, int initAge,String initCity,String initSign) {
-        forename = initForename;
-        surname = initSurname;
-        age = initAge;
-        city = initCity;
-        sign = initSign;
+    public Person(String forename, String surname, int age) {
+        this.forename = forename;
+        this.surname = surname;
+        this.age = age;
     }
 
     // metody klasy określające akcje możliwe do wykonania przez obiekt
     public void hiToAll(){
-        System.out.println("Nazywam się " + forename + " " + surname + ". " + "Mam " + age + " lat. Mieszkam w " + city + " . Mój znak zodiaku to "+ sign + " .");
+        System.out.println("Nazywam się " + forename + " " + surname + ". " + "Mam " + age + " lat.");
     }
 
     // przykładowa metoda growOld, która zwiększa dany wiek obiektu o jeden za każdym jej wywołaniem
-    public int growOld(int years){
-        age = age + years;
+    public int growOld(int age){
+        this.age += age;
         return age;
-    }
-    int beYounger() {
-        age = age - 1;
-        return age;
-    }
-    // przykładowa metoda getName zwracająca wartość forename obiektu
-        String getName() {
-            return forename;
     }
 
-    // przykładowa metoda setName ustawiająca wartość forename obiektu
-    public void setName(String nameToSet){
-        forename = nameToSet;
+    //tzw. "gettery" i "settery" pól prywatnych
+    public String getForename(){
+        return forename;
+    }
+    public void setForename(String forename){
+        this.forename = forename;
+    }
+    public String getSurname(){
+        return surname;
+    }
+    public void setSurname(String surname){
+        this.surname = surname;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setAge(int age){
+        this.age = age;
     }
 }
+
